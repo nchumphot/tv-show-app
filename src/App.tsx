71bef918a89418b4e06dfa7fbaff3b6a6d4a5f24
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-11-04 17:41:09 +0000

    Added PageHeader and episodeList sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import { greet } from "./utils/greet";
+import episodes from "./episodes.json";
+import { episodeList } from "./components/EpisodeItem";
+import { PageHeader } from "./components/PageHeader";
+
+console.log(`Imported ${episodes.length} episode(s)`);
+console.log(`First episode's name is ${episodes[0].name}`);
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <>
+      <PageHeader />
+      {episodeList}
+    </>
+  );
 }
 
 export default App;
