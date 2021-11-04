ef214e3eba607e47e0ee2610e70b00d92a58addb
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-11-04 17:41:36 +0000

    Initial commit

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,10 @@
+export function PageHeader(): JSX.Element {
+  return (
+    <>
+      <h1>Game of Thrones</h1>
+      <p>
+        Data obtained from <a href="https://tvmaze.com/">TVMaze.com</a>
+      </p>
+    </>
+  );
+}
