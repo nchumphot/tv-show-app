1568ae69e682502831b81adecaa3e7f978c6179d
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-11-10 16:32:29 +0000

    Added grid structure

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -2,22 +2,20 @@ import React, { useEffect, useState } from "react";
 import IEpisode from "./IEpisode";
 import { numberCorrector } from "../utils/numberCorrector";
 import { htmlTagRemover } from "../utils/htmlTagRemover";
-import { showMySearch } from "../utils/showMySearch";
 
 export function MainContent(): JSX.Element {
   const [episodes, setEpisodes] = useState<IEpisode[]>([]);
   const [mySearch, setMySearch] = useState<string>("");
   const [mySelected, setMySelected] = useState<string>("");
-  const [episodeToShow, setEpisodeToShow] = useState<IEpisode[]>(episodes);
+  const [episodesToShow, setEpisodesToShow] = useState<IEpisode[]>([]);
+
   // Fetching data from API
   useEffect(() => {
     const fetchEpisodes = async () => {
-      const response = await fetch(
-        "https://api.tvmaze.com/shows/22036/episodes"
-      );
+      const response = await fetch("https://api.tvmaze.com/shows/82/episodes");
       const jsonBody: IEpisode[] = await response.json();
       setEpisodes(jsonBody);
-      setEpisodeToShow(jsonBody);
+      setEpisodesToShow(jsonBody);
     };
     fetchEpisodes();
   }, []);
@@ -28,36 +26,61 @@ export function MainContent(): JSX.Element {
         <input
           type="search"
           value={mySearch}
-          placeholder="Search"
+          placeholder="Search..."
           onChange={(e) => {
             setMySearch(e.target.value);
-            setEpisodeToShow(
-              showMySearch(episodes, e.target.value, mySelected)
-            );
+            setEpisodesToShow(filterEpisodes(e.target.value, ""));
           }}
         />
         <p>
-          Displaying {episodeToShow.length}/{episodes.length} episodes
+          Displaying {episodesToShow.length}/{episodes.length} episodes
         </p>
       </>
     );
   };
   const EpisodeItem = (props: IEpisode): JSX.Element => {
     return (
-      <section key={props.id}>
+      <div key={props.id}>
         <h2>
           {props.name} - S{numberCorrector(props.season)}E
           {numberCorrector(props.number)}
         </h2>
-        {props.image !== null && <img src={props.image.medium} alt="" />}
+        {props.image !== null && (
+          <img src={props.image.medium} alt="" className="center" />
+        )}
         <p>{htmlTagRemover(props.summary)}</p>
-      </section>
+      </div>
     );
   };
+
+  const filterEpisodes = (mySearch: string, mySelected: string): IEpisode[] => {
+    return episodes
+      .filter((item) => {
+        // filter by search
+        if (mySearch === "") {
+          return item;
+        } else {
+          if (item.name.toLowerCase().includes(mySearch.toLowerCase())) {
+            return item;
+          } else if (
+            item.summary.toLowerCase().includes(mySearch.toLowerCase())
+          ) {
+            return item;
+          } else {
+            return null;
+          }
+        }
+      })
+      .filter((item) => {
+        // filter by drop-down selection
+        return !mySelected ? item : item.name === mySelected && item;
+      });
+  };
+
   const EpisodeList = (): JSX.Element => {
     return (
       <>
-        {episodeToShow.map(
+        {episodesToShow.map(
           (episode: IEpisode): JSX.Element => EpisodeItem(episode)
         )}
       </>
@@ -79,10 +102,10 @@ export function MainContent(): JSX.Element {
     setMySearch(""); // When the drop-down is used, set mySearch to an empty string
     if (e.target.value === "Choose from a drop-down list") {
       setMySelected("");
-      setEpisodeToShow(episodes);
+      setEpisodesToShow(filterEpisodes(mySearch, ""));
     } else {
       setMySelected(e.target.value);
-      setEpisodeToShow(showMySearch(episodes, "", e.target.value));
+      setEpisodesToShow(filterEpisodes(mySearch, e.target.value));
     }
   };
   const DropDownList = (): JSX.Element => {
@@ -94,7 +117,6 @@ export function MainContent(): JSX.Element {
         </select>
         <button
           onClick={() => {
-            setEpisodeToShow(episodes);
             setMySearch("");
             setMySelected("");
           }}
@@ -108,14 +130,14 @@ export function MainContent(): JSX.Element {
   // console.log for debugging
   console.log("My search is:", mySearch);
   console.log("MY selection is:", mySelected);
-  console.log("There are ", episodeToShow.length, " matching searches.");
 
   return (
     <>
       <DropDownList />
-      <br></br>
-      <br></br>
+      <br />
+      <br />
       <SearchBar />
+      <hr />
       <div className="grid-container">
         <EpisodeList />
       </div>
