9997beb9a984438492c1a671c7989e5fe181690f
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-11-10 15:08:29 +0000

    Imported CSS file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { PageHeader } from "./components/PageHeader";
 import { MainContent } from "./components/MainContent";
+import "./css/style.css";
 
 // console.log(`Imported ${episodes.length} episode(s)`);
 // console.log(`First episode's name is ${episodes[0].name}`);
